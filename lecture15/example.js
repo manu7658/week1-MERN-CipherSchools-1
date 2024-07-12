f9de0eb42cb57e430733206daadee3442f021b96
Uday <udayr5386@gmail.com>
2024-07-12 23:43:41 +0530

    first commit

---
+++ lecture15/example.js
@@ -0,0 +1,40 @@
+// Strings
+let word = "raining"; //String.Prototy[e]
+// console.log(word.toUpperCase()); //RAINING
+console.log(typeof "Hello");
+console.log("Hello and good evening".length);
+console.log("Hello".charAt(4));
+
+// boolean (true(1),false(0))
+// True value any value other than 0
+// False value 0, false,'',NaN, undefined
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof isNaN("five"));
+
+/* if(1){
+        console.log("Very True ! ")
+} else{
+        console.log("Very Wrong ! ")
+} */
+
+if ("hello") {
+  console.log("very true");
+} else {
+  console.log("very wrong");
+}
+console.log("hello".includes("e"));
+
+//Null Undefined
+let score1;
+let result1 = null;
+
+// console.log(typeof(score1));
+// console.log(typeof(result1));
+
+if (result1) {
+  console.log("very true");
+} else {
+  console.log("very wrong");
+}
